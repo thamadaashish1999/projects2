255e1f38f5bf1e59ec2c31dea68017bca0735c21
thamadaashish1999 <thamadaa.cse@sbjit.edu.in>
2020-12-28 11:34:45 +0530

    Admin

---
+++ app/src/main/java/com/example/admin_section/charity_to_second_activity.java
@@ -0,0 +1,14 @@
+package com.example.admin_section;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class charity_to_second_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charity_to_second_activity);
+    }
+}
---
+++ app/src/main/java/com/example/admin_section/donor_to_second_activity.java
@@ -0,0 +1,14 @@
+package com.example.admin_section;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class donor_to_second_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_to_second_activity);
+    }
+}
