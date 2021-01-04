b7ed81b4f299b56fa280628c583b75e8f4c62705
thamadaashish1999 <thamadaa.cse@sbjit.edu.in>
2021-01-04 11:44:19 +0530

    admin

--- app/src/main/java/com/example/admin_section/signup_page.java
+++ app/src/main/java/com/example/admin_section/signup_page.java
@@ -1,20 +1,14 @@
 package com.example.admin_section;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
-public class donor_to_second_activity extends AppCompatActivity {
-
-
+public class signup_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_donor_to_second_activity);
-
-
+        setContentView(R.layout.activity_signup_page);
     }
 }
