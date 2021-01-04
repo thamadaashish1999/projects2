6f8c7af41fb957999eea41777a8c5cf7fc7f01c3
thamadaashish1999 <thamadaa.cse@sbjit.edu.in>
2021-01-04 13:55:29 +0530

    admin

--- app/src/main/java/com/example/admin_section/login_page.java
+++ app/src/main/java/com/example/admin_section/login_page.java
@@ -2,13 +2,32 @@ package com.example.admin_section;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login_page extends AppCompatActivity {
+
+    Button signUP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        signUP=(Button)findViewById(R.id.signUP);
+
+        signUP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+    }
+    public void openActivity(){
+        Intent intent = new Intent(this, signup_page.class);
+        startActivity(intent);
     }
 }
